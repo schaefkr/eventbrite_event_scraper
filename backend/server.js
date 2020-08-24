8727af0ac9da757456ff1259b7afc5c7910406c0
schaefkr <schaefkr@oregonstate.edu>
2020-08-24 20:59:56 +0200

    initial website setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public/css')));
+app.use(express.static(path.join(__dirname, '../public/js')));
+app.use(express.static(path.join(__dirname, '../public/assets/img')));
+
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+
+app.listen(port, () => {
+    console.log('server is listening on port ', port);
+});
