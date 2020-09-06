50252625684d2cc6842f742a40bfd9681b8e36b6
schaefkr <schaefkr@oregonstate.edu>
2020-09-06 20:21:02 +0200

    added webscaper for techno events

---
+++ backend/eventScraper.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer');
+
+async function scrapeEvents(url) {
+
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    // get array of event lis from Eventbrite and return array of objects containing eventName, eventDate and eventURL
+    const eventData = await page
+        .waitForSelector('div[class="search-main-content"] > ul[class="search-main-content__events-list"] > li')
+        .then(() =>
+            page.evaluate(() => {
+                const eventsList = [];
+                const eventsNodeList = document.querySelectorAll('div[class="search-main-content"] > ul[class="search-main-content__events-list"] > li');
+                eventsNodeList.forEach(eventNode => {
+                    const eventName = eventNode.querySelector('div > div > div > div > div > div > article > div[class="eds-event-card-content__content-container eds-l-pad-right-2"] > div > div > div > a > h3 > div > div').innerText;
+                    const eventDate = eventNode.querySelector('div > div > div > div > div > div > article > div[class="eds-event-card-content__content-container eds-l-pad-right-2"] > div > div > div > div').innerText;
+                    const eventURL = eventNode.querySelector('div > div > div > div > div > div > article > div[class="eds-event-card-content__content-container eds-l-pad-right-2"] > div > div > div > a').getAttribute('href');
+                    const eventLocation = eventNode.querySelector('div > div > div > div > div > div > article > div[class="eds-event-card-content__content-container eds-l-pad-right-2"] > div > div > div[class="eds-event-card-content__sub-content"] > div > div').innerText;
+                    eventsList.push({eventName, eventDate, eventURL, eventLocation});
+                });
+                return eventsList;
+            })
+        )
+        .catch(() => console.log('Selector Error'));
+
+    await browser.close();
+    return eventData;
+}
+
+module.exports = {
+    scrapeEvents
+}
--- backend/server.js
+++ backend/server.js
@@ -2,33 +2,44 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
-const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+const eventScraper = require('./eventScraper');
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // set up handlebars and view engine
+app.set('views', path.join(__dirname, '../views'));
 app.engine(
     'handlebars',
     exphbs({
-        defaultLayout: 'main'
+        defaultLayout: 'main',
     })
 );
-//test app.set line below
-app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'handlebars');
 
-
+// serve static files
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../public/css')));
 app.use(express.static(path.join(__dirname, '../public/js')));
 app.use(express.static(path.join(__dirname, '../public/assets/img')));
 
+// get routes
+app.get('/', async (req, res) => {
+    // add resident advisor events
+    // const eventURL = https://www.residentadvisor.net/events/nl/amsterdam;
+    const eventURL = 'https://www.eventbrite.com/d/netherlands--amsterdam/techno-music/';
+    const eventData = await eventScraper.scrapeEvents(eventURL);
 
-app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {
+       technoEvents: eventData
+    });
 });
 
+// post routes
+
+
+// set up port
 app.listen(port, () => {
     console.log('server is listening on port ', port);
 });
